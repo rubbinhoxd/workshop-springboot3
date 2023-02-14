c1df1085d86161fc80d55d23ad678d70b37b4009
rubbinhoxd <fracicorubens@yahoo.com>
2023-02-14 16:09:27 -0300

    criacao banco de dados h2-console

--- src/main/java/com/projetoBackEnd/project/entities/User.java
+++ src/main/java/com/projetoBackEnd/project/entities/User.java
@@ -1,9 +1,13 @@
 package com.projetoBackEnd.project.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
