303dea6aeef21131041c0c9d3fba24a7985cce5e
rubbinhoxd <fracicorubens@yahoo.com>
2023-02-17 15:31:17 -0300

    CRUD completed User

--- src/main/java/com/projetoBackEnd/project/resources/UserResource.java
+++ src/main/java/com/projetoBackEnd/project/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.projetoBackEnd.project.entities.User;
 import com.projetoBackEnd.project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,6 +16,12 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody User obj){
+        obj = service.insertUser(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
         List<User> list = service.findAll();
@@ -26,6 +31,16 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.updateUser(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.deleteUserById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/projetoBackEnd/project/services/UserService.java
+++ src/main/java/com/projetoBackEnd/project/services/UserService.java
@@ -14,12 +14,31 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+
+    public User insertUser(User obj){
+        return repository.save(obj);
+    }
     public List<User> findAll(){
         return repository.findAll();
     }
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
         return obj.get();
+    }
+    public User updateUser(Long id, User obj){
+        User entity = repository.getReferenceById(id); //referencia, mas so deixa o obj monitorado, ou seja, nao vai ao banco
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
+    public void deleteUserById(Long id){
+        repository.deleteById(id);
     }
 
 }
